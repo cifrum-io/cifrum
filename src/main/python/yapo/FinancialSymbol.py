0aee70b787848ce5098f803dc9902677fbe0b02c
Alexander Myltsev <alexander@myltsev.com>
2017-06-04 19:11:00 +0300

    Rename `asset` to `financial_sybmol`

--- src/main/python/yapo/FinancialSymbol.py
+++ src/main/python/yapo/FinancialSymbol.py
@@ -7,7 +7,7 @@ from yapo.Enums import Currency, SecurityType, Period
 from yapo import Settings
 
 
-class Asset:
+class FinancialSymbol:
     def __init__(self, namespace, ticker, values,
                  isin=None,
                  short_name=None,
@@ -33,15 +33,15 @@ class Asset:
         return self.values()
 
 
-class AssetsSource:
+class FinancialSymbolsSource:
     def __init__(self, namespace):
         self.namespace = namespace
 
-    def get_assets(self):
+    def get_financial_symbols(self):
         raise Exception('should not be called')
 
 
-class SingleItemAssetsSource(AssetsSource):
+class SingleFinancialSymbolSource(FinancialSymbolsSource):
     def __init__(self, path, namespace, ticker,
                  isin=None,
                  short_name=None,
@@ -56,71 +56,72 @@ class SingleItemAssetsSource(AssetsSource):
         self.ticker = ticker
 
         url = Settings.rostsber_url + self.path
-        self.asset = Asset(namespace=self.namespace,
-                           ticker=self.ticker,
-                           values=lambda: pd.read_csv(url, sep='\t'),
-                           isin=isin,
-                           short_name=short_name,
-                           long_name=long_name,
-                           exchange=exchange,
-                           currency=currency,
-                           security_type=security_type,
-                           period=period,
-                           adjusted_close=adjusted_close)
+        self.financial_symbol = FinancialSymbol(namespace=self.namespace,
+                                                ticker=self.ticker,
+                                                values=lambda: pd.read_csv(url, sep='\t'),
+                                                isin=isin,
+                                                short_name=short_name,
+                                                long_name=long_name,
+                                                exchange=exchange,
+                                                currency=currency,
+                                                security_type=security_type,
+                                                period=period,
+                                                adjusted_close=adjusted_close)
 
-    def get_assets(self):
-        return [self.asset]
+    def get_financial_symbols(self):
+        return [self.financial_symbol]
 
 
-class MicexStocksAssetsSource(AssetsSource):
+class MicexStocksFinancialSymbolsSource(FinancialSymbolsSource):
     def __init__(self):
         super().__init__('micex')
         self.url_base = Settings.rostsber_url + 'moex/stock_etf/'
         self.index = pd.read_csv(self.url_base + 'stocks_list.csv', sep='\t')
 
-    def get_assets(self):
-        assets = []
+    def get_financial_symbols(self):
+        symbols = []
         for (idx, row) in self.index.iterrows():
             secid = row['SECID']
-            asset = Asset(namespace=self.namespace,
-                          ticker=secid,
-                          values=lambda: pd.read_csv(self.url_base + secid + '.csv', sep='\t'),
-                          exchange='MICEX',
-                          short_name=row['SHORTNAME'],
-                          long_name=row['NAME'],
-                          isin=row['ISIN'],
-                          currency=Currency.RUB,
-                          security_type=SecurityType.STOCK_ETF,
-                          period=Period.DAY,
-                          adjusted_close=True)
-            assets.append(asset)
-        return assets
+            symbol = FinancialSymbol(namespace=self.namespace,
+                                     ticker=secid,
+                                     values=lambda: pd.read_csv(self.url_base + secid + '.csv',
+                                                                sep='\t'),
+                                     exchange='MICEX',
+                                     short_name=row['SHORTNAME'],
+                                     long_name=row['NAME'],
+                                     isin=row['ISIN'],
+                                     currency=Currency.RUB,
+                                     security_type=SecurityType.STOCK_ETF,
+                                     period=Period.DAY,
+                                     adjusted_close=True)
+            symbols.append(symbol)
+        return symbols
 
 
-class NluAssetsSource(AssetsSource):
+class NluFinancialSymbolsSource(FinancialSymbolsSource):
     def __init__(self):
         super().__init__('nlu')
         self.url_base = Settings.rostsber_url + 'mut_rus/'
         self.index = pd.read_csv(self.url_base + 'mut_rus.csv', sep='\t')
 
-    def get_assets(self):
-        assets = []
+    def get_financial_symbols(self):
+        symbols = []
         for (idx, row) in self.index.iterrows():
             url = '{}/{}'.format(self.url_base, row['id'])
-            asset = Asset(namespace=self.namespace,
-                          ticker=str(row['id']),
-                          values=lambda: pd.read_csv(url, sep='\t'))
-            assets.append(asset)
-        return assets
+            symbol = FinancialSymbol(namespace=self.namespace,
+                                     ticker=str(row['id']),
+                                     values=lambda: pd.read_csv(url, sep='\t'))
+            symbols.append(symbol)
+        return symbols
 
 
-class AssetsRegistry(object):
+class FinancialSymbolsRegistry(object):
     quandl.ApiConfig.api_key = os.environ['QUANDL_KEY']
 
-    def __init__(self, asset_sources):
-        self.assets = []
-        for asset_source in asset_sources:
-            self.assets += asset_source.get_assets()
+    def __init__(self, symbol_sources):
+        self.symbols = []
+        for symbol_source in symbol_sources:
+            self.symbols += symbol_source.get_financial_symbols()
 
     def get(self, namespace, ticker):
         if namespace == 'quandl':
@@ -131,12 +132,12 @@ class AssetsRegistry(object):
                 df_res['date'] = df_res.index
                 return df_res
 
-            asset = Asset(namespace=namespace, ticker=ticker, values=extract_values)
-            return asset
+            symbol = FinancialSymbol(namespace=namespace, ticker=ticker, values=extract_values)
+            return symbol
         else:
             result = list(filter(
                 lambda ast: ast.namespace == namespace and ast.ticker == ticker,
-                self.assets
+                self.symbols
             ))
             if len(result) != 1:
                 raise Exception('ticker {}/{} is not found'.format(namespace, ticker))
@@ -144,9 +145,9 @@ class AssetsRegistry(object):
             return result[0]
 
 
-class AssetSourceContainer(containers.DeclarativeContainer):
+class FinancialSymbolsSourceContainer(containers.DeclarativeContainer):
     currency_usd_rub_source = providers.Singleton(
-        SingleItemAssetsSource,
+        SingleFinancialSymbolSource,
         namespace='cbr',
         ticker='USD',
         path='currency/USD-RUB.csv',
@@ -158,7 +159,7 @@ class AssetSourceContainer(containers.DeclarativeContainer):
     )
 
     inflation_ru_source = providers.Singleton(
-        SingleItemAssetsSource,
+        SingleFinancialSymbolSource,
         namespace='infl',
         ticker='RU',
         path='inflation_ru/data.csv',
@@ -169,29 +170,29 @@ class AssetSourceContainer(containers.DeclarativeContainer):
         adjusted_close=False
     )
 
-    inflation_eu_source = providers.Singleton(SingleItemAssetsSource,
+    inflation_eu_source = providers.Singleton(SingleFinancialSymbolSource,
                                               path='inflation_eu/data.csv',
                                               namespace='infl',
                                               ticker='EU')
 
-    micex_mcftr_source = providers.Singleton(SingleItemAssetsSource,
+    micex_mcftr_source = providers.Singleton(SingleFinancialSymbolSource,
                                              path='moex/mcftr/data.csv',
                                              namespace='micex',
                                              ticker='MCFTR')
 
-    micex_stocks_source = providers.Singleton(MicexStocksAssetsSource)
+    micex_stocks_source = providers.Singleton(MicexStocksFinancialSymbolsSource)
 
-    nlu_muts_source = providers.Singleton(NluAssetsSource)
+    nlu_muts_source = providers.Singleton(NluFinancialSymbolsSource)
 
-    assets_registry = providers.Singleton(AssetsRegistry,
-                                          asset_sources=[
-                                              currency_usd_rub_source(),
-                                              inflation_ru_source(),
-                                              inflation_eu_source(),
-                                              micex_mcftr_source(),
-                                              micex_stocks_source(),
-                                              nlu_muts_source(),
-                                          ])
+    financial_symbols_registry = providers.Singleton(FinancialSymbolsRegistry,
+                                                     symbol_sources=[
+                                                         currency_usd_rub_source(),
+                                                         inflation_ru_source(),
+                                                         inflation_eu_source(),
+                                                         micex_mcftr_source(),
+                                                         micex_stocks_source(),
+                                                         nlu_muts_source(),
+                                                     ])
 
 
 def info(ids: str):
@@ -207,7 +208,8 @@ def info(ids: str):
     tickers_info = []
     for id_str in ids_arr:
         ticker_namespace, ticker = id_str.split('/')
-        asset = AssetSourceContainer.assets_registry().get(ticker_namespace, ticker)
-        tickers_info.append(asset)
+        registry = FinancialSymbolsSourceContainer.financial_symbols_registry()
+        ticker_info = registry.get(ticker_namespace, ticker)
+        tickers_info.append(ticker_info)
     tickers_info = tickers_info[0] if len(tickers_info) == 1 else tickers_info
     return tickers_info
