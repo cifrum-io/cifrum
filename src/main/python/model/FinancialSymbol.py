07304d4a611325da86c2d1d499d51f8ed3a40b0c
Alexander Myltsev <alexander@myltsev.com>
2017-10-23 22:08:00 +0300

    Transform dates to periods inside FinSym

--- src/main/python/model/FinancialSymbol.py
+++ src/main/python/model/FinancialSymbol.py
@@ -13,9 +13,26 @@ class FinancialSymbol:
                  security_type=None,
                  period=None,
                  adjusted_close=None):
+        def values_transformer():
+            if period == Period.DAY:
+                pass
+            elif period == Period.MONTH:
+                return values()
+            else:
+                pass  # Consider what to do with other periods
+
+            vals = values()
+            vals['date'] = pd.to_datetime(vals['date'])
+            vals['period'] = vals['date'].dt.to_period('M')
+            vals_lastdate_indices = vals.groupby(['period'])['date'].transform(max) == vals['date']
+            vals = vals[vals_lastdate_indices]
+            vals.index = vals['period']
+            del vals['date'], vals['period']
+            return vals
+
         self.namespace = namespace
         self.ticker = ticker
-        self.values = values
+        self.values = values_transformer
         self.isin = isin
         self.short_name = short_name
         self.long_name = long_name
@@ -28,16 +45,6 @@ class FinancialSymbol:
     def get_table(self, start_period, end_period):
         start_period = pd.Period(start_period, freq='M')
         end_period = pd.Period(end_period, freq='M')
-
-        vals = self.values()
-        vals['date'] = pd.to_datetime(vals['date'])
-        vals['period'] = vals['date'].dt.to_period('M')
-        vals_lastdate_indices = vals.groupby(['period'])['date'].transform(max) == vals['date']
-        vals = vals[vals_lastdate_indices]
-
-        vals = vals[(vals['period'] >= start_period) & (vals['period'] < end_period)]
-        vals.index = vals['period']
-        del vals['date'], vals['period']
 
         dt = DataTable(financial_symbol=self, values=vals)
         return dt
