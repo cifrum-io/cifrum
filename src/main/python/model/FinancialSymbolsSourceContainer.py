1b6732fb73dfd0103273e5a77bed4922308515b8
Alexander Myltsev <alexander@myltsev.com>
2017-10-23 21:13:00 +0300

    Minor styling fixes

--- src/main/python/model/FinancialSymbolsSourceContainer.py
+++ src/main/python/model/FinancialSymbolsSourceContainer.py
@@ -9,65 +9,60 @@ class FinancialSymbolsSourceContainer(containers.DeclarativeContainer):
         SingleFinancialSymbolSource,
         namespace='infl',
         ticker='RU',
-        values_fetcher=lambda:
-            pd.read_csv(Settings.rostsber_url + 'inflation_ru/data.csv', sep='\t'),
+        values_fetcher=lambda: pd.read_csv(Settings.rostsber_url + 'inflation_ru/data.csv', sep='\t'),
         short_name='Инфляция РФ',
         currency=Currency.RUB,
         security_type=SecurityType.INFLATION,
         period=Period.MONTH,
-        adjusted_close=False
+        adjusted_close=False,
     )
 
     inflation_eu_source = providers.Singleton(
         SingleFinancialSymbolSource,
         namespace='infl',
         ticker='EU',
-        values_fetcher=lambda:
-            pd.read_csv(Settings.rostsber_url + 'inflation_eu/data.csv', sep='\t'),
+        values_fetcher=lambda: pd.read_csv(Settings.rostsber_url + 'inflation_eu/data.csv', sep='\t'),
         short_name='Инфляция ЕС',
         currency=Currency.EUR,
         security_type=SecurityType.INFLATION,
         period=Period.MONTH,
-        adjusted_close=False
+        adjusted_close=False,
     )
 
     inflation_us_source = providers.Singleton(
         SingleFinancialSymbolSource,
         namespace='infl',
         ticker='US',
-        values_fetcher=lambda:
-            pd.read_csv(Settings.rostsber_url + 'inflation_us/data.csv', sep='\t'),
+        values_fetcher=lambda: pd.read_csv(Settings.rostsber_url + 'inflation_us/data.csv', sep='\t'),
         short_name='Инфляция США',
         currency=Currency.USD,
         security_type=SecurityType.INFLATION,
         period=Period.MONTH,
-        adjusted_close=False
+        adjusted_close=False,
     )
 
     cbr_top_rates_source = providers.Singleton(
         SingleFinancialSymbolSource,
         namespace='cbr',
         ticker='TOP_rates',
-        values_fetcher=lambda:
-            pd.read_csv(Settings.rostsber_url + 'cbr_deposit_rate/data.csv', sep='\t'),
+        values_fetcher=lambda: pd.read_csv(Settings.rostsber_url + 'cbr_deposit_rate/data.csv', sep='\t'),
         long_name='Динамика максимальной процентной ставки (по вкладам в российских рублях) ',
         currency=Currency.RUB,
         security_type=SecurityType.RATES,
         period=Period.DECADE,
-        adjusted_close=False
+        adjusted_close=False,
     )
 
     micex_mcftr_source = providers.Singleton(
         SingleFinancialSymbolSource,
         namespace='micex',
         ticker='MCFTR',
-        values_fetcher=lambda:
-            pd.read_csv(Settings.rostsber_url + 'moex/mcftr/data.csv', sep='\t'),
+        values_fetcher=lambda: pd.read_csv(Settings.rostsber_url + 'moex/mcftr/data.csv', sep='\t'),
         short_name='MICEX Total Return',
         currency=Currency.RUB,
         security_type=SecurityType.INDEX,
         period=Period.DAY,
-        adjusted_close=False
+        adjusted_close=False,
     )
 
     micex_stocks_source = providers.Singleton(MicexStocksFinancialSymbolsSource)
@@ -87,6 +82,6 @@ class FinancialSymbolsSourceContainer(containers.DeclarativeContainer):
             micex_mcftr_source(),
             micex_stocks_source(),
             nlu_muts_source(),
-            quandl_source()
+            quandl_source(),
         ]
     )
