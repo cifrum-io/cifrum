a0fa8d1d2bf1dcf1f83ecb5d56808be8bef09774
Alexander Myltsev <alexander@myltsev.com>
2017-11-02 20:11:00 +0300

    Cache DataTable values

--- src/unittest/python/datatable_tests.py
+++ src/unittest/python/datatable_tests.py
@@ -9,20 +9,28 @@ from model.Enums import Currency
 
 
 class DataTableTest(unittest.TestCase):
-    sample_symbol_names = ['quandl/MSFT', 'micex/SBER', 'micex/SBERP', 'micex/MCFTR', 'nlu/419',
-                           'cbr/USD', 'cbr/EUR', 'infl/RU', 'infl/US', 'infl/EU']
-
-    def test_values_should_have_correct_schema(self):
-        for symbol_name, currency in itertools.product(DataTableTest.sample_symbol_names, Currency):
-            print(symbol_name, currency)
+    @classmethod
+    def setUpClass(cls):
+        cls.sample_symbol_names = ['quandl/MSFT',
+                                   'micex/SBER', 'micex/SBERP', 'micex/MCFTR',
+                                   'nlu/419',
+                                   'cbr/USD', 'cbr/EUR', 'cbr/RUB',
+                                   'infl/RU', 'infl/US', 'infl/EU']
+        cls.symbols_data = {}
+        for symbol_name, currency in itertools.product(cls.sample_symbol_names, Currency):
             sym = yapo.information(symbol_name)
             dt = sym.get_table(start_period='2010-1', end_period='2015-6', currency=currency.name)
+            cls.symbols_data.update({(symbol_name, currency.name): dt})
+
+    def test_values_should_have_correct_schema(self):
+        for symbol_name_currency in self.symbols_data.keys():
+            dt = self.symbols_data[symbol_name_currency]
             self.assertTrue(set(dt.values.columns) >= {'period', 'close'})
 
     def test_index_should_be_numerical(self):
-        for symbol_name, currency in itertools.product(DataTableTest.sample_symbol_names, Currency):
-            sym = yapo.information(symbol_name)
-            dt = sym.get_table(start_period='2010-1', end_period='2015-6', currency=currency.name)
+        for symbol_name_currency in self.symbols_data.keys():
+            dt = self.symbols_data[symbol_name_currency]
+            self.assertTrue(set(dt.values.columns) >= {'period', 'close'})
             self.assertIsInstance(dt.values.index.dtype, PeriodDtype,
-                                  msg='Incorrect index type for ' + symbol_name)
+                                  msg='Incorrect index type for ' + symbol_name_currency[0])
             self.assertIsInstance(dt.values.index.dtype.freq, MonthEnd)
