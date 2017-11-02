23e2b0bd55b9fd70f2bea52b0b48e4e5684ffc70
Alexander Myltsev <alexander@myltsev.com>
2017-11-02 19:49:00 +0300

    Verify schema of DataTable values

--- src/unittest/python/datatable_tests.py
+++ src/unittest/python/datatable_tests.py
@@ -12,6 +12,13 @@ class DataTableTest(unittest.TestCase):
     sample_symbol_names = ['quandl/MSFT', 'micex/SBER', 'micex/SBERP', 'micex/MCFTR', 'nlu/419',
                            'cbr/USD', 'cbr/EUR', 'infl/RU', 'infl/US', 'infl/EU']
 
+    def test_values_should_have_correct_schema(self):
+        for symbol_name, currency in itertools.product(DataTableTest.sample_symbol_names, Currency):
+            print(symbol_name, currency)
+            sym = yapo.information(symbol_name)
+            dt = sym.get_table(start_period='2010-1', end_period='2015-6', currency=currency.name)
+            self.assertTrue(set(dt.values.columns) >= {'period', 'close'})
+
     def test_index_should_be_numerical(self):
         for symbol_name, currency in itertools.product(DataTableTest.sample_symbol_names, Currency):
             sym = yapo.information(symbol_name)
