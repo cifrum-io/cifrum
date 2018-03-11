8072d127eadf3ad876a84200407f73875476e6a3
Alexander Myltsev <alexander@myltsev.com>
2018-03-11 06:45:00 +0300

    Introduce `epsilon` to tests

--- src/unittest/python/portfolio_asset_statistics_tests.py
+++ src/unittest/python/portfolio_asset_statistics_tests.py
@@ -10,6 +10,7 @@ class PortfolioAssetStatisticsTest(unittest.TestCase):
         cls.asset_name = 'quandl/BND'
         cls.asset = yapo.portfolio_asset(name=cls.asset_name,
                                          start_period='2011-1', end_period='2017-2', currency='USD')
+        cls.epsilon = 1e-3
 
     def test_accumulated_rate_of_return(self):
         arors = self.asset.accumulated_rate_of_return()[1:]
@@ -17,16 +18,16 @@ class PortfolioAssetStatisticsTest(unittest.TestCase):
 
     def test_compound_annual_growth_rate(self):
         cagr_default = self.asset.compound_annual_growth_rate()
-        self.assertTrue(abs(cagr_default - 0.03) < 1e-3)
+        self.assertTrue(abs(cagr_default - 0.03) < self.epsilon)
 
         cagr_long_time = self.asset.compound_annual_growth_rate(years_ago=20)
-        self.assertTrue(abs(cagr_default - cagr_long_time) < 1e-3)
+        self.assertTrue(abs(cagr_default - cagr_long_time) < self.epsilon)
 
         cagr_one_year = self.asset.compound_annual_growth_rate(years_ago=1)
-        self.assertTrue(abs(cagr_one_year - .012) < 1e-3)
+        self.assertTrue(abs(cagr_one_year - .012) < self.epsilon)
 
         cagr_array = self.asset.compound_annual_growth_rate(years_ago=[None, 20, 1])
-        cagr_diff = np.abs(cagr_array - [cagr_default, cagr_long_time, cagr_one_year]) < 1e-3
+        cagr_diff = np.abs(cagr_array - [cagr_default, cagr_long_time, cagr_one_year]) < self.epsilon
         self.assertTrue(np.all(cagr_diff))
 
     def test_risk(self):
@@ -35,7 +36,7 @@ class PortfolioAssetStatisticsTest(unittest.TestCase):
                                  start_period='2016-8', end_period='2016-12', currency='USD')
 
         self.assertRaises(Exception, short_asset.risk, period='year')
-        self.assertTrue(abs(self.asset.risk() - .0311) < 1e-3)
-        self.assertTrue(abs(self.asset.risk(period='year') - .0311) < 1e-3)
-        self.assertTrue(abs(self.asset.risk(period='month') - .0087) < 1e-3)
+        self.assertTrue(abs(self.asset.risk() - .0311) < self.epsilon)
+        self.assertTrue(abs(self.asset.risk(period='year') - .0311) < self.epsilon)
+        self.assertTrue(abs(self.asset.risk(period='month') - .0087) < self.epsilon)
 
--- src/unittest/python/portfolio_statistics_tests.py
+++ src/unittest/python/portfolio_statistics_tests.py
@@ -10,11 +10,12 @@ class PortfolioStatisticsTest(unittest.TestCase):
         cls.asset_names = {'quandl/BND': .4, 'quandl/VTI': .4, 'quandl/VXUS': .2}
         cls.portfolio = yapo.portfolio(assets=cls.asset_names,
                                        start_period='2011-1', end_period='2017-2', currency='USD')
+        cls.epsilon = 1e-3
 
     def test_rate_of_return(self):
         rate_of_return_naive = \
             sum(asset.rate_of_return() * weight for asset, weight in self.portfolio.assets_weighted())
-        self.assertTrue(np.all(np.abs(self.portfolio.rate_of_return() - rate_of_return_naive) < 1e-3))
+        self.assertTrue(np.all(np.abs(self.portfolio.rate_of_return() - rate_of_return_naive) < self.epsilon))
 
     def test_accumulated_rate_of_return(self):
         arors = self.portfolio.accumulated_rate_of_return()[1:]
@@ -22,16 +23,16 @@ class PortfolioStatisticsTest(unittest.TestCase):
 
     def test_compound_annual_growth_rate(self):
         cagr_default = self.portfolio.compound_annual_growth_rate()
-        self.assertTrue(abs(cagr_default - .182) < 1e-3)
+        self.assertTrue(abs(cagr_default - .182) < self.epsilon)
 
         cagr_long_time = self.portfolio.compound_annual_growth_rate(years_ago=20)
-        self.assertTrue(abs(cagr_default - cagr_long_time) < 1e-3)
+        self.assertTrue(abs(cagr_default - cagr_long_time) < self.epsilon)
 
         cagr_one_year = self.portfolio.compound_annual_growth_rate(years_ago=1)
-        self.assertTrue(abs(cagr_one_year - .475) < 1e-3)
+        self.assertTrue(abs(cagr_one_year - .475) < self.epsilon)
 
         cagr_array = self.portfolio.compound_annual_growth_rate(years_ago=[None, 20, 1])
-        cagr_diff = np.abs(cagr_array - [cagr_default, cagr_long_time, cagr_one_year]) < 1e-3
+        cagr_diff = np.abs(cagr_array - [cagr_default, cagr_long_time, cagr_one_year]) < self.epsilon
         self.assertTrue(np.all(cagr_diff))
 
     def test_risk(self):
@@ -40,6 +41,6 @@ class PortfolioStatisticsTest(unittest.TestCase):
                            start_period='2016-8', end_period='2016-12', currency='USD')
 
         self.assertRaises(Exception, short_portfolio, period='year')
-        self.assertTrue(abs(self.portfolio.risk() - .078) < 1e-3)
-        self.assertTrue(abs(self.portfolio.risk(period='year') - .078) < 1e-3)
-        self.assertTrue(abs(self.portfolio.risk(period='month') - .021) < 1e-3)
+        self.assertTrue(abs(self.portfolio.risk() - .078) < self.epsilon)
+        self.assertTrue(abs(self.portfolio.risk(period='year') - .078) < self.epsilon)
+        self.assertTrue(abs(self.portfolio.risk(period='month') - .021) < self.epsilon)
