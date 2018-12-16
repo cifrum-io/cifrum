Fix ROR_YTD computation

src/main/python/yapo/model/Portfolio.py
@@ -152,14 +152,23 @@ class PortfolioAsset(PortfolioInflation):
         if kind not in ['values', 'accumulated', 'ytd']:
             raise ValueError('`kind` is not in expected values')
 
-        ror = self.values.pct_change()
-
         if kind == 'ytd':
-            ror_ytd = ror[-self.period_max.month:]
-            if real:
-                inflation = self.inflation(kind='values')[-self.period_max.month:]
-                ror_ytd = (ror_ytd + 1.) / (inflation + 1).cumprod() - 1.
-            return (ror_ytd + 1.).cumprod() - 1.
+            ror = self.rate_of_return(kind='values', real=real)
+
+            drop_first_count = 0 if ror.start_period.month == 1 else _MONTHS_PER_YEAR - ror.start_period.month + 1
+            drop_last_count = 0 if ror.end_period.month == _MONTHS_PER_YEAR else ror.end_period.month
+            ror_full_yearly = ror[drop_first_count:-drop_last_count]
+            ror_full_yearly_splits = \
+                np.split(ror_full_yearly.values,
+                         ror_full_yearly.end_period.year - ror_full_yearly.start_period.year + 1)
+            ror_ytd_ts = [((splt + 1.).prod() - 1.) for splt in ror_full_yearly_splits]
+            ror_ytd = TimeSeries(values=np.array(ror_ytd_ts),
+                                 start_period=ror_full_yearly.start_period,
+                                 end_period=ror_full_yearly.end_period,
+                                 kind=[TimeSeriesKind.VALUES, TimeSeriesKind.DIFF, TimeSeriesKind.YTD])
+            return ror_ytd
+
+        ror = self.values.pct_change()
 
         if kind == 'accumulated':
             ror = (ror + 1.).cumprod() - 1.

src/unittest/python/portfolio_asset_statistics_tests.py
@@ -29,16 +29,17 @@ class PortfolioAssetStatisticsTest(unittest.TestCase):
 
     def test__ytd_rate_of_return(self):
         ror_ytd = self.asset.rate_of_return(kind='ytd')
-        self.assertEqual(ror_ytd.start_period,
-                         pd.Period(year=self.portfolio_period_end.year, month=1, freq='M'))
-        self.assertEqual(ror_ytd.end_period, self.portfolio_period_end)
-        self.assertAlmostEqual(ror_ytd.values[-1], -.0434, places=self.places)
+        self.assertEqual(ror_ytd.start_period, pd.Period('2012-1', freq='M'))
+        self.assertEqual(ror_ytd.end_period, pd.Period('2016-12', freq='M'))
+        self.assertEqual(ror_ytd.kind, TimeSeriesKind.YTD)
+        np.testing.assert_almost_equal(ror_ytd.values, [0.2041, -0.0344, -0.4531, 0.0046, 0.5695], decimal=self.places)
 
         ror_ytd_real = self.asset.rate_of_return(kind='ytd', real=True)
-        self.assertEqual(ror_ytd_real.start_period,
-                         pd.Period(year=self.portfolio_period_end.year, month=1, freq='M'))
-        self.assertEqual(ror_ytd_real.end_period, self.portfolio_period_end)
-        self.assertAlmostEqual(ror_ytd_real.values[-1], -.0574, places=self.places)
+        self.assertEqual(ror_ytd_real.start_period, pd.Period('2012-1', freq='M'))
+        self.assertEqual(ror_ytd_real.end_period, pd.Period('2016-12', freq='M'))
+        self.assertEqual(ror_ytd_real.kind, TimeSeriesKind.YTD)
+        np.testing.assert_almost_equal(ror_ytd_real.values,
+                                       [-0.2696, -0.5081, -0.7703, -0.5840, -0.4408], decimal=self.places)
 
     def test__handle_related_inflation(self):
         self.assertRaises(Exception, self.asset.inflation, kind='abracadabra')