bb6b69ec03d57e5024c24324a7838d2014bc957d
Alexander Myltsev <alexander@myltsev.com>
2019-05-27 11:19:05 +0300

    OKID10: extract start/end period computation from the loop

--- yapo/_index/okid10.py
+++ yapo/_index/okid10.py
@@ -6,11 +6,13 @@ import numpy as np
 
 def compute(cbr_top10_rates):
     index_total = None
+    start_period = min(cbr_top10_rates['period'])
+    end_period = max(cbr_top10_rates['period'])
     for month_idx in range(_MONTHS_PER_YEAR):
         rates_yearly = cbr_top10_rates['rate'].values[month_idx::_MONTHS_PER_YEAR]
         index_part = TimeSeries(values=np.repeat(rates_yearly, _MONTHS_PER_YEAR)[:len(cbr_top10_rates) - month_idx],
-                                start_period=min(cbr_top10_rates['period']) + month_idx + 1,
-                                end_period=max(cbr_top10_rates['period']) + 1,
+                                start_period=start_period + month_idx + 1,
+                                end_period=end_period + 1,
                                 kind=TimeSeriesKind.VALUES)
         index_part = (1 + index_part / _MONTHS_PER_YEAR).cumprod()
         index_total = index_part if index_total is None else index_total[1:] + index_part
