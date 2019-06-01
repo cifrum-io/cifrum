d6b89a157a3710c4ef394d3c9a45480f0087a893
Alexander Myltsev <alexander@myltsev.com>
2019-06-01 23:57:20 +0300

    Use v0.1.3 data scheme

--- yapo/_settings.py
+++ yapo/_settings.py
@@ -2,5 +2,5 @@ import os
 
 _MONTHS_PER_YEAR = 12
 
-data_url = os.environ.get('DATA_URL', 'https://okama.io/api/data/')
+data_url = os.environ.get('DATA_URL', 'https://okama.io/api/data-v0.1.3/')
 change_column_name = 'close_pctchange'
